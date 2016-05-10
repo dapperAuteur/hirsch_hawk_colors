e68c1c40401b5f03d9496f4b9390b1577f3d904b
dapperauteur <anthony.mcdonald@asu.edu>
2016-05-10 02:21:24 +0000

    Create static pages, application layout and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+
+  get 'help', to: 'static_pages#help'
+  
+  get 'about', to: 'static_pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select 'title', 'Home | Welcome To Hirsch Hawk Colors'
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select 'title', 'Help | Welcome To Hirsch Hawk Colors'
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select 'title', 'About | Welcome To Hirsch Hawk Colors'
+  end
+
+end
