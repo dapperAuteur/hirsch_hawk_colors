ca4ee4b68a785e9fb04e02fd894bdb9e2a8593e8
dapperauteur <anthony.mcdonald@asu.edu>
2016-05-17 04:45:57 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Hirsch Hawk Colors!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#home'
+  root                   'static_pages#home'
 
-  get 'help',     to: 'static_pages#help'
+  get 'help',       to:  'static_pages#help'
   
-  get 'about',    to: 'static_pages#about'
+  get 'about',      to:  'static_pages#about'
   
-  get 'contact',  to: 'static_pages#contact'
+  get 'contact',    to:  'static_pages#contact'
   
-  get 'signup',   to: 'users#new'
+  get 'signup',     to:  'users#new'
   
-  resources           :users
+  get 'login',      to:  'sessions#new'
+  
+  post 'login',     to:  'sessions#create'
+  
+  delete 'logout',  to:  'sessions#destroy'
+  
+  resources              :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
